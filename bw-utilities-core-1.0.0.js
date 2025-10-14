71efaf107daf1d8b868076bca5f9bfa729e22179
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-10-14 12:09:10 -0300

    feat: add initial implementation of BedWars Utilities core module

---
+++ bw-utilities-core-1.0.0.js
@@ -0,0 +1,27 @@
+const BedWarsUtilities = require("./bw-utilities-src/BedWarsUtilities");
+const configSchema = require("./bw-utilities-src/config/configSchema");
+
+module.exports = (api) => {
+  process.on("uncaughtException", (err, origin) => {
+    console.error(`[BWU FATAL] UNHANDLED ERROR: ${err.stack}`);
+  });
+
+  api.metadata({
+    name: "bwu",
+    displayName: "BedWars Utilities",
+    prefix: "§6BWU",
+    version: "1.0.0",
+    author: "Grille (silly_brazil)",
+    description:
+      "A versatile Bedwars plugin offering a variety of useful features to enhance gameplay.",
+  });
+
+  const bwu = new BedWarsUtilities(api);
+
+  api.initializeConfig(configSchema);
+  api.configSchema(configSchema);
+
+  bwu.registerHandlers();
+  return bwu;
+};
+
